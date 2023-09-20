64dff64e5eac971641bbab03825e260ead9b6fad
omballm2 <mubarak.omballi2@mail.dcu.ie>
2023-09-20 17:35:17 +0100

    Added event handler for uploading pdf

--- components/PdfUpload.tsx
+++ components/PdfUpload.tsx
@@ -1,13 +1,31 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 
 const PdfUpload = () => {
+    const [selectedFile, setSelectedFile] = useState("");
+
+    const handleFileChange = (event: any) => {
+        setSelectedFile(event.target.files[0]);
+    };
+
+    const handleUpload = async () => {
+        if (!selectedFile) {
+            alert("Please select a PDF file to upload!");
+            return;
+        }
+
+        const formData = new FormData();
+        formData.append("pdfFile", selectedFile);
+    };
+
     return (
         <div className="p-10">
             <form>
                 <h1 className="mb-4">Upload a PDF file</h1>
-                <input type="file" accept="application/pdf" />
+                <input type="file" accept="application/pdf" onChange={handleFileChange} />
                 <div className="mt-4">
-                    <button className="p-2 border-teal-500 border-4 rounded-2xl text-sm" type="submit">
+                    <button className="p-2 border-teal-500 border-4 rounded-2xl text-sm" type="submit" onClick={handleUpload}>
                         Upload
                     </button>
                 </div>
